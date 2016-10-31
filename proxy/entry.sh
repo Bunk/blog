6b642f0c720adace7773ea52e0fc711f26590045
JD Courtoy <jd.courtoy@gmail.com>
2016-10-31 13:54:42 -0500

    Fixed an issue with the proxy not running a shell correctly

--- proxy/entry.sh
+++ proxy/entry.sh
@@ -3,7 +3,7 @@ set -e
 
 # Generate the Caddyfile based on the following heredoc template
 cat >${CADDY_FILE} <<EOF
-${CADDY_PROXY_DOMAINS:-"*.courtoy.io"}
+${CADDY_PROXY_DOMAINS:-"localhost"}
 proxy / ${CADDY_PROXY_TO:-"blog:2368"} {
   transparent
 }
@@ -11,5 +11,4 @@ expvar /stats
 tls ${CADDY_TLS:-off}
 EOF
 
-exec /usr/bin/caddy -conf ${CADDY_FILE} -log stdout -agree $@
-# exec /bin/sh
+exec $@
